d265a905f4866a1832bc2f29d3165aadb5644c5f
Vlad Mehakovic <vlad.mehakovic@X-Vlad-Mehakovic-MBP13.local>
2015-03-30 08:27:22 +1100

    Revised heroku db connection

--- config/database.rb
+++ config/database.rb
@@ -1,8 +1,8 @@
-DATABASE_URL = $(heroku config:get DATABASE_URL -a compi) if Padrino.env == :development
+# DATABASE_URL = $(heroku config:get DATABASE_URL -a compi) if Padrino.env == :development
 Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect(DATABASE_URL, :loggers => [logger])
+  when :development then Sequel.connect(ENV['DATABASE_URL'], :loggers => [logger])
   when :production  then Sequel.connect("postgres://localhost/compi_production",  :loggers => [logger])
   when :test        then Sequel.connect("postgres://localhost/compi_test",        :loggers => [logger])
 end
