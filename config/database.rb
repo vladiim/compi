ed1a639b27e66551e4e93806dd216bc79b78e936
Vlad Mehakovic <vlad.mehakovic@X-Vlad-Mehakovic-MBP13.local>
2015-03-31 15:31:39 +1100

    Fixed Padrino db connection error

--- config/database.rb
+++ config/database.rb
@@ -1,8 +1,15 @@
 # DATABASE_URL = $(heroku config:get DATABASE_URL -a compi) if Padrino.env == :development
 Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
+
+# Sequel::Model.db = if Padrino.env == :development || :test
+#     Sequel.connect("postgres://localhost/compi_#{ Padrino.env.to_s }", :loggers => [logger])
+#   else
+#     Sequel.connect(ENV['DATABASE_URL'])
+# end
+
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect(ENV['DATABASE_URL'])
-  when :production  then Sequel.connect("postgres://localhost/compi_production",  :loggers => [logger])
+  when :production then Sequel.connect(ENV['DATABASE_URL'], loggers: [logger])
+  when :development  then Sequel.connect("postgres://localhost/compi_development",  :loggers => [logger])
   when :test        then Sequel.connect("postgres://localhost/compi_test",        :loggers => [logger])
 end
