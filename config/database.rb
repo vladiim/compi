07eaae9d9b16f0eb2888ad73fe1559412fbe774f
Vlad Mehakovic <vlad.mehakovic@X-Vlad-Mehakovic-MBP13.local>
2015-03-31 15:18:36 +1100

    Trying db connection w/out logger

--- config/database.rb
+++ config/database.rb
@@ -2,7 +2,7 @@
 Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect(ENV['DATABASE_URL'], :loggers => [logger])
+  when :development then Sequel.connect(ENV['DATABASE_URL'])
   when :production  then Sequel.connect("postgres://localhost/compi_production",  :loggers => [logger])
   when :test        then Sequel.connect("postgres://localhost/compi_test",        :loggers => [logger])
 end
