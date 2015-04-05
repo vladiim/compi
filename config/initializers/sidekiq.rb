cc28f704cec9fc3887f3f824bc9f94d961fb24c8
Vlad Mehakovic <vlad.mehakovic@X-Vlad-Mehakovic-MBP13.local>
2015-04-05 16:00:09 +1000

    Sidekiq not working on heroku #bug

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@ require 'sidekiq'
 REDIS_CONFIG = { size: 1, url: ENV['REDISTOGO_URL'] }
 
 Sidekiq.configure_client do |config|
-  config.redis = REDIS_CONFIG if Padrino.env == :production
+  config.redis = REDIS_CONFIG.merge(size: 1) if Padrino.env == :production
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = REDIS_CONFIG if Padrino.env == :production
+  config.redis = REDIS_CONFIG.merge(size: 2) if Padrino.env == :production
 end
